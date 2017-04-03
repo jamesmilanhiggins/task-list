c35a0bd1eba8de0c8a0362c10fea3c6e46bc145b
James Higgins and Tanner Eustice <james+tanner@>
2017-04-03 11:58:37 -0700

    add jquery draggable and sortable

--- js/scripts.js
+++ js/scripts.js
@@ -9,12 +9,23 @@ $(function(){
     e.preventDefault();
     var userTask = $("#task").val();
 
-
     var newTask = new Task(userTask);
     $("#outputSection").append("<li class='task'>"
     + userTask
-    + "<button class='remove btn btn-danger'>Completed</button>" 
-    + "<hr> </li>" )
+    + "<button class='remove btn btn-danger'>Completed</button>"
+    + "<hr> </li>" );
+
+    $("#outputSection").sortable({
+      revert: true
+    });
+    $( ".task" ).draggable({
+      connectToSortable: "#outputSection",
+      revert: "invalid"
+    });
+
+    $( "ul, li" ).disableSelection();
+
+
 
     // $(".task").last().click(function(){
     //   $("#show-place").show();
@@ -24,12 +35,12 @@ $(function(){
     //   $("#show-place .notes").text(userNotes);
     //
     // });
-
     $(".remove").last().click(function(){
-        $(this).parent().toggleClass("completed");
+      $(this).parent().toggleClass("completed");
+      $(this).parent().appendTo("#outputSection");
+      $(this).parent().toggle().fadeIn(200);
     });
 
     $("#task").val("");
-
   });
 });

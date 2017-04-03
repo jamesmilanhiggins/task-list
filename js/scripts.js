9b779753b2b36f871800e8d7b932c1e0a81ae520
James Higgins and Tanner Eustice <james+tanner@>
2017-04-03 10:07:07 -0700

    create task list application, displays and removes tasks

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,32 @@
 //Back End
-function Place (location, landmarks, timeOfYear, notes ) {
-  this.location = location;
-  this.landmarks = landmarks;
-  this.timeOfYear = timeOfYear;
-  this.notes = notes;
+function Task (task ) {
+  this.task = task;
 };
 
 //Front End
 $(function(){
   $("form").submit(function(e){
     e.preventDefault();
-    var userLocation = $("#location").val();
-    var userLandmarks = $("#landmarks").val();
-    var userTimeOfYear = $("#timeOfYear").val();
-    var userNotes = $("#notes").val();
+    var userTask = $("#task").val();
 
-    var newPlace = new Place(userLocation, userLandmarks, userTimeOfYear, userNotes);
-    $("#outputSection").append("<li class='place'>" + userLocation + "</li>")
 
-    $(".place").last().click(function(){
-      $("#show-place").show();
-      $("#show-place h2").text(userLocation);
-      $("#show-place .landmarks").text(userLandmarks);
-      $("#show-place .timeOfYear").text(userTimeOfYear);
-      $("#show-place .notes").text(userNotes);
+    var newTask = new Task(userTask);
+    $("#outputSection").append("<li class='task'>" + userTask + "<button class='remove btn btn-danger'>Completed</button>" + "<hr> </li>" )
+
+    // $(".task").last().click(function(){
+    //   $("#show-place").show();
+    //   $("#show-place h2").text(userLocation);
+    //   $("#show-place .landmarks").text(userLandmarks);
+    //   $("#show-place .timeOfYear").text(userTimeOfYear);
+    //   $("#show-place .notes").text(userNotes);
+    //
+    // });
+
+    $(".remove").last().click(function(){
+        $(this).parent().remove();
     });
-    $("#location").val("");
-    $("#landmarks").val("");
-    $("#timeOfYear").val("");
-    $("#notes").val("");
+
+    $("#task").val("");
 
   });
 });
